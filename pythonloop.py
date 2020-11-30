6808133149c8e22f799248f183988de56b9b3bd1
Abhiraj Singh Bisht <67232823+AbhirajSB@users.noreply.github.com>
2020-11-30 16:59:03 +0100

    Update pythonloop.py

--- pythonloop.py
+++ pythonloop.py
@@ -6,3 +6,6 @@ for itervar in [3, 41, 12, 9, 74, 15]:
         break
     print("Loop:", itervar, smallest)
 print("Smallest:", smallest)
+
+#The answer will be 3
+#But the site will show you that the correct answer is 6 in the quiz for some reason.
